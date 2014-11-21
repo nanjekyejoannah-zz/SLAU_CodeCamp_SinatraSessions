9a05c34ddcf9fe7100ce9ce85c6d4673e73bb27c
nanjekyejoannah <nanjekyejoannah@yahoo.com>
2014-11-21 15:06:24 +0300

    added CRUD

--- JoannahNanjekye_SinatraSession/app.rb
+++ JoannahNanjekye_SinatraSession/app.rb
@@ -7,11 +7,11 @@ DataMapper::setup(:default,"sqlite3://#{Dir.pwd}/attendee.db")
 #creating a table
 class Attendee
 	include DataMapper::Resource
-	property: id, Serial
-	property: name, Text
-	property: corse,Text
+	property:id, Serial
+	property:name, Text
+	property:course,Text
 
-	DataMapper.finalize.auto_upgrade!
+	DataMapper.auto_upgrade!
 end
 
 
@@ -27,4 +27,19 @@ end
 get '/contact' do 
 
 	erb :contact
+end
+
+
+get '/new' do
+	erb :new
+end
+
+post '/new' do
+Attendee.create(:name => params[:name] , :course => params[:course])
+redirect '/'
+end
+
+get '/attendee_list' do
+	@attendees = Attendee.all 
+	erb :attendee_list
 end
